e10402f1ec93eaa418bd19b0d927c344a93945f4
Robin Bertuccelli <robin.bertuccelli@gmail.com>
2018-03-07 14:31:30 -0600

    Gonna commit before branching out

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(45) NULL,
+devoured BOOLEAN NOT NULL DEFAULT 0,
+PRIMARY KEY  (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT  INTO burgers (burger_name, devoured)
+VALUES ("cheeseburger", 0), ("bacon cheeseburger", 0), ("tex-mex burger", 0) ;
+  SELECT * FROM burgers;
